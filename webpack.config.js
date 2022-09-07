09921c57553819d2fee8830eccc7f6256038f359
Amitedri <amitedri778@gmail.com>
2022-09-07 12:43:42 +0300

    updates

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 module.exports = {
-    resolve: {
-        alias: {
-            path: require.resolve("path-browserify")
-        }
-    }
+  resolve: {
+    alias: {
+      path: require.resolve('path-browserify'),
+    },
+    fallback: { path: require.resolve('path-browserify') },
+  },
 };
